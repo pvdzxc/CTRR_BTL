b5408350637717ff9deac5b84ccdc1b96511acd1
pvdzxc <pvdzxc@gmail.com>
2022-03-10 11:53:29 +0700

    thêm file dữ liệu

--- test.R
+++ test.R
@@ -1,28 +1,26 @@
 library(stringi)
 setwd("F://PvD//Studying//Programming//CTRRAssignment//CTRR_BTL")
 dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
-
-        x <- length(dataRaw$continent)
+#MADE 4263
+  x <- length(dataRaw$continent)
+  
 #(1) Years of research
   curYear <- "2000"                 #current Year
   cntYear <- 0                      #counter Year (0 default)
   Years <- {}                       #Years listing
   iCountry <- dataRaw$iso_code[1]   #checking Years analyzed in 1st country
-  
   for (i in 1 : 1000)
   {
     if (iCountry != dataRaw$iso_code[i])
       break;
     if (curYear != stri_sub(dataRaw$date[i], -4))
     {
-      cntYear = cntYear + 1
-      Years[cntYear] = stri_sub(dataRaw$date[i], -4)
-      curYear = stri_sub(dataRaw$date[i], -4)
+      cntYear <- cntYear + 1
+      Years[cntYear] <- stri_sub(dataRaw$date[i], -4)
+      curYear <- stri_sub(dataRaw$date[i], -4)
     }
   }
   write.table(Years, file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = ",")
-
-
 
 #(2) Print first 10 countries
 {
@@ -34,32 +32,32 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
     {
       if (curCountry != dataRaw$iso_code[i] && stri_sub(dataRaw$iso_code[i], 0, 4) != "OWID")
       {
-        cntCountry = cntCountry + 1
-        curCountry = dataRaw$iso_code[i]
-        Country[cntCountry] = dataRaw$location[i]
-        Iso_code[cntCountry] = curCountry
+        cntCountry <- cntCountry + 1
+        curCountry <- dataRaw$iso_code[i]
+        Country[cntCountry] <- dataRaw$location[i]
+        Iso_code[cntCountry] <- curCountry
       }
       if (cntCountry == 10)
         break
     }
     list_first10Country <- data.frame(Iso_code, Country)
-    write.table(list_first10Country, file = "task1.csv", row.names = FALSE, quote = FALSE, sep = ",", append = TRUE)
+    write.table(list_first10Country, file="task1.csv", row.names = FALSE, quote = FALSE, sep = ",", append = TRUE)
 }
 
 #(2) Counting number of Countries
-{
+  {
     cntCountry <- 1     #counter Counties
     curCountry <- "AFG" #current Country
     for (i in 1 : x)
     {
       if (curCountry != dataRaw$iso_code[i] && stri_sub(dataRaw$iso_code[i], 0, 4) != "OWID")
       {
-        cntCountry = cntCountry + 1
-        curCountry = dataRaw$iso_code[i]
+        cntCountry <- cntCountry + 1
+        curCountry <- dataRaw$iso_code[i]
       }
     }
     write.table(cntCountry, file = "task1.csv", row.names = FALSE, col.names = FALSE, append = TRUE)
-}
+  }
   
 #(3) Data saved each continent
 {
@@ -78,14 +76,14 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
         {
           if (dataRaw$continent[i] == Continent[j])
           {
-            isValid = 0; break
+            isValid <- 0; break
           }
         }
         if (isValid != 0) 
         {
-          curCon = dataRaw$continent[i]
-          cntCon = cntCon + 1
-          Continent[cntCon] = curCon
+          curCon <- dataRaw$continent[i]
+          cntCon <- cntCon + 1
+          Continent[cntCon] <- curCon
         }
       }
     }
@@ -98,41 +96,40 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
   {
     if (Continent[i] == "Africa")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Châu Phi"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Châu Phi"
     }
     if (Continent[i] == "Asia")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Châu Á"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Châu Á"
     }
     if (Continent[i] == "Europe")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Châu Âu"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Châu Âu"
     }
     if (Continent[i] == "South America")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Nam Mỹ"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Nam Mỹ"
     }
     if (Continent[i] == "North America")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Bắc Mỹ"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Bắc Mỹ"
     }
     if (Continent[i] == "Oceania")
     {
-      cntCon_Vie = cntCon_Vie + 1
-      Continent_Vie[cntCon_Vie] = "Châu Đại Dương"
+      cntCon_Vie <- cntCon_Vie + 1
+      Continent_Vie[cntCon_Vie] <- "Châu Đại Dương"
     }
   }
   write.table(data.frame(Continent, Continent_Vie), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE, sep = ",")
 
   vector1 <- c(0,0,0,0,0,0)
-  Observations <- array(vector1, dim = 6)  #Data counter
+  Observations <- array(vector1, dim <- 6)  #Data counter
 }
-
 #(4) data counted continents
 {
   x <- length(dataRaw$continent)
@@ -145,7 +142,7 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
         {
           if (dataRaw$continent[i] == Continent[j])
           {
-            Observations[j] = Observations[j] + 1; break;
+            Observations[j] <- Observations[j] + 1; break;
           }
         }
       }
@@ -155,7 +152,7 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
   sumData <- 0
   for (j in 1 : (cntCon))
   {
-    sumData = sumData + Observations[j]
+    sumData <- sumData + Observations[j]
   }
   write.table(paste("Tổng:", sumData), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
 }
@@ -171,13 +168,13 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
     {
       if (Observations[j] < fewestcontinent)
       {
-        fewestcontinent = Observations[j]
-        posfewestcontinent = j
+        fewestcontinent <- Observations[j]
+        posfewestcontinent <- j
       }
       if (Observations[j] > mostcontinent)
       {
-        mostcontinent = Observations[j]
-        posmostcontinent = j
+        mostcontinent <- Observations[j]
+        posmostcontinent <- j
       }
     }
   }
@@ -201,12 +198,12 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
         if (dataRaw$location[i] != curCountry)
         {
           if (cntCountry == 10) break
-          cntCountry = cntCountry + 1
-          curCountry = dataRaw$location[i]
-          Countries[11 - cntCountry] = dataRaw$location[i]
-          Observations[11 - cntCountry] = 0
+          cntCountry <- cntCountry + 1
+          curCountry <- dataRaw$location[i]
+          Countries[11 - cntCountry] <- dataRaw$location[i]
+          Observations[11 - cntCountry] <- 0
         }
-        Observations[11 - cntCountry] = Observations[11 - cntCountry] + 1
+        Observations[11 - cntCountry] <- Observations[11 - cntCountry] + 1
       }
     }
   write.table(data.frame(Countries, Observations), file = "task1.csv", row.names = FALSE, quote = FALSE, append = TRUE, sep = ",")
@@ -225,21 +222,21 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
       {
         if (dataRaw$location[i] != curCountry)
         {
-          cntCountry = cntCountry + 1
-          curCountry = dataRaw$location[i]
-          Countries[cntCountry] = dataRaw$location[i]
-          Observations[cntCountry] = 0
+          cntCountry <- cntCountry + 1
+          curCountry <- dataRaw$location[i]
+          Countries[cntCountry] <- dataRaw$location[i]
+          Observations[cntCountry] <- 0
         }
-        Observations[cntCountry] = Observations[cntCountry] + 1
+        Observations[cntCountry] <- Observations[cntCountry] + 1
       }
     }
   sumData <- 0
   for (j in 1 : cntCountry)
   {
-    sumData = sumData + Observations[j]
+    sumData <- sumData + Observations[j]
   }
   write.table(paste("Tổng:", sumData), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
-  }
+}
 
 #(8,9) fewest and most data counted countries
 {
@@ -247,23 +244,89 @@ dataRaw <- read.csv2("owid-covid-data.csv", header = TRUE, sep = ",")
   posfewestcountry <- 0
   mostcountry <- 0
   posmostcountry <- 0
-  for (j in 1 : (cntCon))
+  for (j in 1 : (10))
   {
     {
       if (Observations[j] < fewestcountry)
       {
-        fewestcountry = Observations[j]
-        posfewestcountry = j
+        fewestcountry <- Observations[j]
+        posfewestcountry <- j
       }
       if (Observations[j] > mostcountry)
       {
-        mostcountry = Observations[j]
-        posmostcountry = j
+        mostcountry <- Observations[j]
+        posmostcountry <- j
       }
     }
   }
+
   write.table(paste("Nước ít dữ liệu nhất:", Countries[posfewestcountry], fewestcountry), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
+
   write.table(paste("Nước nhiều dữ liệu nhất:", Countries[posmostcountry], mostcountry), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
 }
 
-#
+#(10,11) datee
+{
+  x <- length(dataRaw$date)
+  curdate <- "VietnamNumber1"
+  cntdate <- 0
+  date <- {}
+  Observations <- {}
+  basedate <- strptime("1/1/2020", format = "%m/%d/%Y")
+  everyday <- seq(as.Date("2020-1-1"), as.Date("2022-8-1"), by="days")
+  for (i in 1 : 1000)
+  Observations[i] <- 0
+  for (i in 1 : x)
+  {
+    k <- as.integer(strptime(dataRaw$date[i], format = "%m/%d/%Y") - basedate)
+    Observations[k] <- Observations[k] + 1
+  }
+  fewestdate <- 1000000
+  posfewestdate <- 0
+  mostdate <- 0
+  cntmdate <- 0
+  firstmdate <- 0
+  lastmdate <- 0
+  for (j in 1 : 1000)
+  {
+    {
+      if (Observations[j] < fewestdate && Observations[j] > 0)
+      {
+        fewestdate <- Observations[j]
+        posfewestdate <- j
+      }
+      if (Observations[j] > mostdate)
+      {
+        mostdate <- Observations[j]
+      }
+    }
+  }
+  for (j in 2 : 943)
+  {
+    k <- as.integer(strptime(everyday[j], format = "%Y-%m-%d") - basedate)
+    if (Observations[k] == fewestdate)
+    {
+      posfewestdate <- everyday[j]
+    }
+    if (Observations[k] == mostdate)
+    {
+      if (cntmdate == 0)
+      {firstmdate <- everyday[j]}
+      cntmdate <- 1
+    }
+    else
+    {
+      if (cntmdate == 1)
+      {
+        lastmdate <- everyday[j-1]
+        break
+      }
+    }
+  }
+  
+  write.table(paste("Ngày ít dữ liệu nhất:", fewestdate), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
+  write.table((posfewestdate), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
+  write.table(paste("Ngày nhiều dữ liệu nhất:", mostdate), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
+  write.table(seq(firstmdate, lastmdate, by="days"), file = "task1.csv", row.names = FALSE, col.names = FALSE, quote = FALSE, append = TRUE)
+  
+}
